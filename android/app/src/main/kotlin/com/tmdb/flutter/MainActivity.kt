5e1be99010d12ddd8494096b1491e41e45292851
winechit-nfq <wine.cht@nfq.com>
2025-05-21 14:36:59 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/tmdb/flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tmdb.flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:tmdb_flutter/app/app.dart';
+import 'package:tmdb_flutter/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
