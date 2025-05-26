bca75e2d61a9e5de3bb8db342b4c9e2a8189e6d8
winechit-dev <winechitpaing.dev@gmail.com>
2025-05-26 13:50:15 +0700

    Refactor movie response models for consistent field order and add genre IDs to Movie model

--- lib/app/data/remote/models/movie_responses.dart
+++ lib/app/data/remote/models/movie_responses.dart
@@ -4,13 +4,6 @@ part 'movie_responses.g.dart';
 
 @JsonSerializable()
 class MoviesResponse {
-  final int page;
-  final List<Movie> results;
-  @JsonKey(name: 'total_pages')
-  final int totalPages;
-  @JsonKey(name: 'total_results')
-  final int totalResults;
-
   MoviesResponse({
     required this.page,
     required this.results,
@@ -20,12 +13,30 @@ class MoviesResponse {
 
   factory MoviesResponse.fromJson(Map<String, dynamic> json) =>
       _$MoviesResponseFromJson(json);
+  final int page;
+  final List<Movie> results;
+  @JsonKey(name: 'total_pages')
+  final int totalPages;
+  @JsonKey(name: 'total_results')
+  final int totalResults;
 
   Map<String, dynamic> toJson() => _$MoviesResponseToJson(this);
 }
 
 @JsonSerializable()
 class Movie {
+  Movie({
+    required this.id,
+    required this.title,
+    required this.voteAverage,
+    required this.overview,
+    this.posterPath,
+    this.backdropPath,
+    this.releaseDate,
+    this.genreIds,
+  });
+
+  factory Movie.fromJson(Map<String, dynamic> json) => _$MovieFromJson(json);
   final int id;
   final String title;
   @JsonKey(name: 'poster_path')
@@ -37,44 +48,33 @@ class Movie {
   @JsonKey(name: 'vote_average')
   final double voteAverage;
   final String overview;
-
-  Movie({
-    required this.id,
-    required this.title,
-    this.posterPath,
-    this.backdropPath,
-    this.releaseDate,
-    required this.voteAverage,
-    required this.overview,
-  });
-
-  factory Movie.fromJson(Map<String, dynamic> json) => _$MovieFromJson(json);
+  @JsonKey(name: 'genre_ids')
+  final List<int>? genreIds;
 
   Map<String, dynamic> toJson() => _$MovieToJson(this);
 }
 
 @JsonSerializable()
 class MovieDetailsResponse extends Movie {
-  final List<Genre> genres;
-  final int runtime;
-  @JsonKey(name: 'production_companies')
-  final List<ProductionCompany> productionCompanies;
-
   MovieDetailsResponse({
     required super.id,
     required super.title,
-    super.posterPath,
-    super.backdropPath,
-    super.releaseDate,
     required super.voteAverage,
     required super.overview,
     required this.genres,
     required this.runtime,
     required this.productionCompanies,
+    super.posterPath,
+    super.backdropPath,
+    super.releaseDate,
   });
 
   factory MovieDetailsResponse.fromJson(Map<String, dynamic> json) =>
       _$MovieDetailsResponseFromJson(json);
+  final List<Genre> genres;
+  final int runtime;
+  @JsonKey(name: 'production_companies')
+  final List<ProductionCompany> productionCompanies;
 
   @override
   Map<String, dynamic> toJson() => _$MovieDetailsResponseToJson(this);
@@ -82,25 +82,17 @@ class MovieDetailsResponse extends Movie {
 
 @JsonSerializable()
 class Genre {
-  final int id;
-  final String name;
-
   Genre({required this.id, required this.name});
 
   factory Genre.fromJson(Map<String, dynamic> json) => _$GenreFromJson(json);
+  final int id;
+  final String name;
 
   Map<String, dynamic> toJson() => _$GenreToJson(this);
 }
 
 @JsonSerializable()
 class ProductionCompany {
-  final int id;
-  final String name;
-  @JsonKey(name: 'logo_path')
-  final String? logoPath;
-  @JsonKey(name: 'origin_country')
-  final String originCountry;
-
   ProductionCompany({
     required this.id,
     required this.name,
@@ -110,43 +102,41 @@ class ProductionCompany {
 
   factory ProductionCompany.fromJson(Map<String, dynamic> json) =>
       _$ProductionCompanyFromJson(json);
+  final int id;
+  final String name;
+  @JsonKey(name: 'logo_path')
+  final String? logoPath;
+  @JsonKey(name: 'origin_country')
+  final String originCountry;
 
   Map<String, dynamic> toJson() => _$ProductionCompanyToJson(this);
 }
 
 @JsonSerializable()
 class GenresResponse {
-  final List<Genre> genres;
-
   GenresResponse({required this.genres});
 
   factory GenresResponse.fromJson(Map<String, dynamic> json) =>
       _$GenresResponseFromJson(json);
+  final List<Genre> genres;
 
   Map<String, dynamic> toJson() => _$GenresResponseToJson(this);
 }
 
 @JsonSerializable()
 class CreditsResponse {
-  final List<Cast> cast;
-  final List<Crew> crew;
-
   CreditsResponse({required this.cast, required this.crew});
 
   factory CreditsResponse.fromJson(Map<String, dynamic> json) =>
       _$CreditsResponseFromJson(json);
+  final List<Cast> cast;
+  final List<Crew> crew;
 
   Map<String, dynamic> toJson() => _$CreditsResponseToJson(this);
 }
 
 @JsonSerializable()
 class Cast {
-  final int id;
-  final String name;
-  final String character;
-  @JsonKey(name: 'profile_path')
-  final String? profilePath;
-
   Cast({
     required this.id,
     required this.name,
@@ -155,19 +145,17 @@ class Cast {
   });
 
   factory Cast.fromJson(Map<String, dynamic> json) => _$CastFromJson(json);
+  final int id;
+  final String name;
+  final String character;
+  @JsonKey(name: 'profile_path')
+  final String? profilePath;
 
   Map<String, dynamic> toJson() => _$CastToJson(this);
 }
 
 @JsonSerializable()
 class Crew {
-  final int id;
-  final String name;
-  final String department;
-  final String job;
-  @JsonKey(name: 'profile_path')
-  final String? profilePath;
-
   Crew({
     required this.id,
     required this.name,
@@ -177,6 +165,12 @@ class Crew {
   });
 
   factory Crew.fromJson(Map<String, dynamic> json) => _$CrewFromJson(json);
+  final int id;
+  final String name;
+  final String department;
+  final String job;
+  @JsonKey(name: 'profile_path')
+  final String? profilePath;
 
   Map<String, dynamic> toJson() => _$CrewToJson(this);
-} 
+}
